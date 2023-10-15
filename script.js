59a4472c5abcd123af9dbbd14464430783e890ee
Owen <134909886+owenKLL@users.noreply.github.com>
2023-10-15 19:32:46 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var spec1 = "https://raw.githubusercontent.com/owenKLL/FIT3179/main/Assessment%202/Scatterplot_fin.json";
+vegaEmbed("#vis1-container", spec1, { "actions": false });
+
+var spec2 = "https://raw.githubusercontent.com/owenKLL/FIT3179/main/Assessment%202/CO2EmissionsBarT10.vl.json";
+vegaEmbed("#vis2-container", spec2, { "actions": false });
+
+var spec3 = "https://raw.githubusercontent.com/owenKLL/FIT3179/main/Assessment%202/stackedareamap.vl.json";
+vegaEmbed("#vis3-container", spec3, { "actions": false });
+
+var spec4 = "https://raw.githubusercontent.com/owenKLL/FIT3179/main/Assessment%202/gdpgrowth.vl.json";
+vegaEmbed("#vis4-container", spec4, { "actions": false });
+
+var spec5 = "https://raw.githubusercontent.com/owenKLL/FIT3179/main/Assessment%202/Access2elec.vl.json";
+vegaEmbed("#vis5-container", spec5, { "actions": false });
